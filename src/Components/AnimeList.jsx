fa2f99b4817016df68e96951bb2c7163807c2bf2
Ariyo Adeyinka <149602253+Ariyoadeyinka@users.noreply.github.com>
2024-05-27 14:24:54 -0700

    New Commit

--- src/Components/AnimeList.jsx
+++ src/Components/AnimeList.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import styles from "../Css/AnimeList.module.css";
 
-export default function AnimeList() {
+export default function AnimeList(props) {
+    const{id, image} =props
   return (
     <div className={styles.animeListContainer}>
-   
+    <img src={image} alt="" />
     </div>
   );
 }
--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,13 +1,40 @@
+import { useState } from "react";
 import styles from "../Css/Search.module.css";
+import AnimeList from "./AnimeList";
 export default function Search({ query, setQuery }) {
+  const [loading, setLoading] = useState(false);
+  const [anime, setAnime] = useState([]);
+
+  const getsearchData = (searchData) => {
+    setLoading(true);
+    async function getAnime() {
+      const apiresponse = await fetch(
+        `https://api.jikan.moe/v4/anime?q=${searchData}&limit=10`
+      );
+      const result = await apiresponse.json();
+
+      const { data } = result;
+      if (data && data.length > 0) {
+        setLoading(false);
+        setAnime(data);
+      }
+    }
+    getAnime();
+  };
+  console.log(loading, anime);
+
   const handleInputValue = (e) => {
     const { value } = e.target;
     setQuery(value);
-    console.log(value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    getsearchData(query);
   };
   return (
     <div>
-      <form className={styles.searchContainer}>
+      <form className={styles.searchContainer} onSubmit={handleSubmit}>
         <input type="text" onChange={handleInputValue} value={query} />
         <button className={styles.Search}>
           <i class="fa-solid fa-magnifying-glass"></i>
@@ -25,6 +52,18 @@ export default function Search({ query, setQuery }) {
           Upcoming <i class="fa-solid fa-calendar-days"></i>
         </button>
       </div>
+      {anime && anime.length > 0
+        ? anime.map((item) => (
+            <AnimeList
+              id={item.mal_id}
+              image={item.images.jpg.image_url}
+              title={item.title}
+              airingStatus={item.status}
+              rank={item.rank}
+              rating={item.rating}
+            />
+          ))
+        : null}
     </div>
   );
 }
