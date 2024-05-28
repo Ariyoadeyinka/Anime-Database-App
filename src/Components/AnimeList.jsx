0e6bdc960f88da0ee73f66b5f4dccdaf8b590a74
Ariyo Adeyinka <149602253+Ariyoadeyinka@users.noreply.github.com>
2024-05-28 10:05:53 -0700

    New Commit

--- src/Components/AnimeList.jsx
+++ src/Components/AnimeList.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import styles from "../Css/AnimeList.module.css";
 
-export default function AnimeList({anime}) {
-
-
+export default function AnimeList({ anime }) {
   return (
     <div className={styles.animeListContainer}>
       <div className="row">
         {anime.map((anime) => (
-          <div key={anime.id} className="col-md-3">
+          <div key={anime.id} className="col-lg-2 col-md-4 col-sm-12">
             <div className={`card ${styles.animeCard}`}>
-              <img src={anime.images.jpg.image_url} className={`card-img-top ${styles.searchedImages}`} alt={anime.title} />
+              <img
+                src={anime.images.jpg.image_url}
+                className={`card-img-top ${styles.searchedImages}`}
+                alt={anime.title}
+              />
               <div className="card-body">
                 <h5 className="card-title">{anime.title}</h5>
               </div>
